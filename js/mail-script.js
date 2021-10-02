c08a80bb960f41f601e8db8bd20964cda69d16cf
shivamsahu12 <73433164+shivamsahu12@users.noreply.github.com>
2021-10-03 03:23:03 +0530

    Update mail-script.js

--- js/mail-script.js
+++ js/mail-script.js
@@ -1,8 +1,8 @@
-    // -------   Mail Send ajax
+   
 
      $(document).ready(function() {
-        var form = $('#myForm'); // contact form
-        var submit = $('.submit-btn'); // submit button
+        var form = $('#myForm'); 
+        var submit = $('.submit-btn'); 
         var alert = $('.alert-msg'); // alert div for show alert message
 
         // form submit event
@@ -10,10 +10,10 @@
             e.preventDefault(); // prevent default form submit
 
             $.ajax({
-                url: 'mail.php', // form action url
+                url: 'mail.php', 
                 type: 'POST', // form submit method get/post
-                dataType: 'html', // request type html/json/xml
-                data: form.serialize(), // serialize form data
+                dataType: 'html', 
+                data: form.serialize(),
                 beforeSend: function() {
                     alert.fadeOut();
                     submit.html('Sending....'); // change submit button text
